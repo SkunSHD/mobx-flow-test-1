f99f4ac9de74edb1189ca241256f11c319fa4307
malyutenko.d <malyutenko.d@gmail.com>
2017-05-08 17:11:46 +0300

    Fix flow errors

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ class App extends Component {
     return (
       <div>
         <button onClick={this.onReset}>
-          Seconds passed: {this.props.appState.timer}
+         Seconds passed: {this.props.appState.timer}
         </button>
         <DevTools />
       </div>
